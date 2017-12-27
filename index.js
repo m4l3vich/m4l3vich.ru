12c25e7c92a2e11c297474c1a6514140e04dc59d
Eugeny Malevich <m4l3vich@mail.ru>
2017-12-28 02:04:32 +0800

    Update website; added projects section

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-  $('.linkcontent').mouseenter(function() {
+/* eslint-env jquery */
+$(document).ready(function () {
+  $('.linkcontent').mouseenter(function () {
     $('#newtab').css('opacity', '0.5')
     clearTimeout(document.timeout)
   })
-  $('.linkcontent').mouseleave(function() {
-    document.timeout = setTimeout(function(){
+  $('.linkcontent').mouseleave(function () {
+    document.timeout = setTimeout(function () {
       $('#newtab').css('opacity', '0')
     }, 1000)
+  })
+
+  $('#downwards').click(function () {
+    $('#downwards').toggleClass('inverted')
+    $('#content').toggleClass('hidden', 'shown')
+    $('#projects').toggleClass('hidden', 'shown')
   })
 })
