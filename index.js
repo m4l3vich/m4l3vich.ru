3c3948a2f04b684da357f80d93bf08bb636d55ea
Eugeny Malevich <m4l3vich@mail.ru>
2018-01-04 23:17:40 +0800

    Added preloader

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Source code is available under MIT License
 // License text is available at https://github.com/m4l3vich/m4l3vich.ru/blob/master/LICENSE.md
 
-$(document).ready(function(){$(".linkcontent").mouseenter(function(){$("#newtab").css("opacity","0.5"),clearTimeout(document.timeout)}),$(".linkcontent").mouseleave(function(){document.timeout=setTimeout(function(){$("#newtab").css("opacity","0")},1e3)}),$("#downwards").click(function(){$("#downwards").toggleClass("inverted"),$("#content").toggleClass("hidden","shown"),$("#projects").toggleClass("hidden","shown")})});
+/* eslint-disable */
+
+$(document).ready(function(){$(".linkcontent").mouseenter(function(){$("#newtab").css("opacity","0.5"),clearTimeout(document.timeout)}),$(".linkcontent").mouseleave(function(){document.timeout=setTimeout(function(){$("#newtab").css("opacity","0")},1e3)}),$("#downwards").click(function(){$("#downwards").toggleClass("inverted"),$("#content").toggleClass("hidden","shown"),$("#projects").toggleClass("hidden","shown")})});$(window).on('load',function(){$('#preloader').delay(500).fadeOut('slow')});
