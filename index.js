1e467dbb73b7d9f019d9afe0ddb0700ddd332d6a
Eugeny Malevich <m4l3vich@mail.ru>
2018-01-04 22:37:42 +0800

    Optimized website

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 // Source code is available under MIT License
 // License text is available at https://github.com/m4l3vich/m4l3vich.ru/blob/master/LICENSE.md
 
-/* eslint-env jquery */
-$(document).ready(function () {
-  $('.linkcontent').mouseenter(function () {
-    $('#newtab').css('opacity', '0.5')
-    clearTimeout(document.timeout)
-  })
-  $('.linkcontent').mouseleave(function () {
-    document.timeout = setTimeout(function () {
-      $('#newtab').css('opacity', '0')
-    }, 1000)
-  })
-
-  $('#downwards').click(function () {
-    $('#downwards').toggleClass('inverted')
-    $('#content').toggleClass('hidden', 'shown')
-    $('#projects').toggleClass('hidden', 'shown')
-  })
-})
+$(document).ready(function(){$(".linkcontent").mouseenter(function(){$("#newtab").css("opacity","0.5"),clearTimeout(document.timeout)}),$(".linkcontent").mouseleave(function(){document.timeout=setTimeout(function(){$("#newtab").css("opacity","0")},1e3)}),$("#downwards").click(function(){$("#downwards").toggleClass("inverted"),$("#content").toggleClass("hidden","shown"),$("#projects").toggleClass("hidden","shown")})});
